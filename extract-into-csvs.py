e9d01870c1c6f2dca9e83b951229b1836f50c724
Ain Salleh <code.ainsalleh@gmail.com>
2023-04-11 12:09:34 +0800

    added more comments and import openpyxl

--- extract-into-csvs.py
+++ extract-into-csvs.py
@@ -6,13 +6,22 @@
 # --------------------------------------
 
 import pandas as pd
-import sys
+import openpyxl
 
+# Use the `input` function to get user input for the file name
 excelname = input("Enter your excel name (E.g Sample_1.xlsx) = ")
 
 try:
-  dfs = pd.read_excel(excelname, sheet_name=None)
-  for sheet_name, data in dfs.items():
-      data.to_csv(f"{sheet_name}.csv", index=False)
-except:
-  print("An error occurred")
+    # Read all sheets from the Excel file into a dictionary of dataframes
+    dfs = pd.read_excel(excelname, sheet_name=None)
+
+    # Loop through each sheet name and corresponding data, and save to a CSV file
+    for sheet_name, data in dfs.items():
+        data.to_csv(f"{sheet_name}.csv", index=False)
+
+    # Print a success message
+    print("CSV files created successfully!")
+    
+except Exception as e:
+    # Print an error message with the specific error that occurred
+    print(f"An error occurred: {e}")
